f4b671cc5b4c12fe5aec7d0902adbb8f195fe32e
Dallas <dcrump011@gmail.com>
2024-07-15 13:47:53 -0700

    Add projects.php file for the 'Projects' sub-page.

---
+++ projects.php
@@ -0,0 +1,72 @@
+<html>
+<link rel="stylesheet" type="text/css" href="Loginc.css">
+<link rel="icon" href="/Files/favicon32.png">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+  body {
+      position: relative; 
+  }
+  
+  .g-recaptcha {
+    transform:scale(0.8);
+    transform-origin:0 0;
+}
+</style>
+
+<meta charset="UTF-8">
+  <title>Dallas Crump</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://www.google.com/recaptcha/api.js"></script>
+
+    <?php
+        require_once "recaptchalib.php";
+    ?>
+  
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<!--Navigation Bar.-->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">  
+    <div class="container-fluid">  
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="https://dallascrump.net/#section1">Intro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://dallascrump.net/#section2">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://dallascrump.net/#section3">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="https://dallascrump.net/projects.php">Projects</a>
+        </li>
+      </ul>
+      <div class="nav navbar-nav navbar-right">
+        <a href="https://www.github.com/dcrump011" target="_blank"><img src="Files/githubLogoOrange.png" style="height: 32px; width: 32px; margin-right: 8px;"></img></a>
+        <a href="https://www.linkedin.com/in/dallas-crump-16b178127/" target="_blank"><img src="Files/LinkedIn_logo.png" style="height: 32px; width: 32px;"></img></a>
+      </div>
+    </div>  
+</nav>
+
+   <div class="jumbotron text-center" style="Height: 110%; margin-top: 40px;"> 
+       <div class="container">
+          <img src="/Files/ticTacToe.png" alt="Avatar" class="image" style="width:60%"> 
+          <div class="middle">
+            <div class="text">John Doe</div>
+          </div>
+        </div>
+    </div>
+<h2 class="test">blah</h2>
+</div>
+</div>
+
+
+
+</body>
+</html>
